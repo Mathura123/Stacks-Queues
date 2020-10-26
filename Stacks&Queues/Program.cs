edb23282d7f2d565666f6b961b5f6fa7d747acdc
Mathura <mathuradas333@gmail.com>
2020-10-26 17:32:43 +0530

    [Mathura]Add . Ability to push values in Stack with push method

--- Stacks&Queues/Program.cs
+++ Stacks&Queues/Program.cs
@@ -8,6 +8,12 @@ namespace Stacks_Queues
         {
             Console.WriteLine("Welcome to Stacks & Queues Problem Assignment");
             Console.WriteLine("=============================================");
+            Stack stackObj = new Stack(new[] { 7, 8, 9 });
+            stackObj.Push(9);
+            stackObj.Push(0);
+            stackObj.Push(-8);
+            stackObj.Push(67);
+            stackObj.Display();
         }
     }
 }
---
+++ Stacks&Queues/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks_Queues
+{
+    class Stack
+    {
+        public Node top;
+        public Stack()
+        { 
+        }
+        public Stack(int[] datas)
+        {
+            foreach(int data in datas)
+            {
+                Push(data);
+            }
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            Node tempNode = top;
+            top = newNode;
+            newNode.next = tempNode;
+            Console.WriteLine($"PUSHED {newNode.data}");
+        }
+        public void Display()
+        {
+            if(top==null)
+                Console.WriteLine("No Data Yet");
+            else
+            {
+                Console.WriteLine("----------------");
+                Console.WriteLine($"At Top: {top.data}");
+                Node tempNode = top;
+                int level = 1;
+                while (tempNode.next!= null)
+                {
+                    Console.WriteLine($"At Level {level} : {tempNode.next.data}");
+                    tempNode = tempNode.next;
+                    level++;
+                }
+                Console.WriteLine("-----------------");
+            }
+        }
+    }
+}
