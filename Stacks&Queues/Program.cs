46b78143aea5b105af95212950577db855b90d1b
Mathura <mathuradas333@gmail.com>
2020-10-26 16:53:15 +0530

    [Mathura]Add. Welcome message added

---
+++ Stacks&Queues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stacks_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks & Queues Problem Assignment");
+            Console.WriteLine("=============================================");
+        }
+    }
+}
